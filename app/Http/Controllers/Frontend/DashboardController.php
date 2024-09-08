dd70d4ecd54e9ec6222243f155e950ca74e19ae2
zafar-m3n <thezafar.m3n@gmail.com>
2024-09-08 11:33:30 +0530

    Controller

--- app/Http/Controllers/Frontend/DashboardController.php
+++ app/Http/Controllers/Frontend/DashboardController.php
@@ -5,13 +5,11 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
-use App\Models\Order;
 
 class DashboardController extends Controller
 {
-    public function index(): View
-    {
-        $orders = Order::where('user_id', auth()->id())->get();
-        return view('frontend.dashboard.index', compact('orders'));
-    }
+    function index() : View {
+        return view('frontend.dashboard.index');
+}
+
 }
